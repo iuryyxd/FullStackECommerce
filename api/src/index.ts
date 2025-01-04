1fbbb3d32e62f2b13bb0bd673548177e36f223d0
iury <iurysena74@gmail.com>
2025-01-04 18:18:31 -0300

    feat: add express json parse

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,14 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import productsRoutes from "./routes/products";
+
 const app = express();
-const PORT = 3000;
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+const port = 3000;
 
 app.use("/products", productsRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
